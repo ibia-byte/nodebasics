ed0be019a2808e4ea77c72579fcbe4f75da05699
ibia-byte <retyibea@yahoo.com>
2019-11-05 10:09:51 +0300

    created hello world using get method by bowser

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+console.log("hello world")
+*/
+const express = require('express');
+const app  = express();
+app.listen(3000, function() {
+console.log('listening on 3000')
+})
+app.get('/', function (req, res){
+    res.send('Hello World')
+})
