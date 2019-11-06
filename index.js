8bb7f7e61d96a156dbca3244bb4ca78ac52359fc
ibia-byte <retyibea@yahoo.com>
2019-11-06 11:13:05 +0300

    POST,GET,DELETE,UPDATE server methods

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ console.log('listening on 3000')
 app.get('/', function (req, res){
     res.send('Hello World')
 })
+app.get('/about', function (req,res){
+    res.send('This page is About us')
+})
+app.post ('/', function (req, res){
+    res.send('Got a POST request')
+})
+app.put ('/', function (req, res){
+    res.send('Got a PUT request')
+})
+app.delete ('/', function (req, res){
+    res.send('Got a DELETE request')
+})
+app.get('/users/:name',function (req, res){
+    res.send('hello' +  req.params.name)
+})
+app.get('*',function(req,res){res.send('oops, The page doesn exist')}
+)
+
+
+
