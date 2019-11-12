d1e8e4143cda7d2025f83b15500fd7f3236d68a3
ibia-byte <retyibea@yahoo.com>
2019-11-12 18:26:47 +0300

    created start pug file and connected db

---
+++ index (2).js
@@ -0,0 +1,108 @@
+const path = require('path')
+const bodyParser = require('body-parser')
+// creates app by calling express
+const express =  require('express');
+const app = express()
+
+// body-parser to parse request body data
+app.use(bodyParser.urlencoded({extended: true}))
+
+//Setting the view engine to pug
+app.set('view engine', 'pug');
+
+//Setting the folder
+app.set('views', path.join(__dirname, 'views'))
+
+//Setting the server to port 3000
+app.listen(3000, () => {
+    console.log('listening on 3000');
+    })
+
+// Mongoose connects the app to the mongoDB
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/node-demo");
+
+// Model
+
+// Creating a Schema
+var nameSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String
+  });
+
+//GET Route
+app.get('/reg', (req, res, next) => {
+    res.render('form');
+})
+//POST Route
+app.post('/reg', (req, res) => {
+    // console.log("Form has been posted");
+    console.log('body', req.body);
+    console.log('Query Params', req.query);
+    // res.send(req.body)   
+})
+
+//Submit Route
+app.post('/submit', (req, res) => {
+    res.send("Hello " + req.body.firstname);
+     res.render('form_data', {
+        fname: req.body.firstname,
+        lname: req.body.lastname,
+        email: req.body.emailaddress,
+        gender: req.body.gender,
+        country: req.body.country,
+        city: req.body.city,
+        password: req.body.password
+    });
+})
+
+app.get('/index', (req, res) => {
+    res.render('index');
+})
+app.get('/data', (req, res) => {
+    res.render('form_data');
+})
+
+
+
+//Sending a GET request
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/about', (req, res) => {
+    res.send('This is the about us page')
+})
+
+//POST Request
+app.post('/', (req, res) => {
+    res.send('Got a POST request')
+})
+
+//PUT Method
+app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user')
+})
+
+//DELETE Method
+app.delete('/user', (req, res) => {
+    res.send('Got a DELETE request at /user')
+})
+
+//Path Params - set them at the path
+app.get('/users/:name', (req, res) => {
+    res.send('Hello ' + req.params.name)
+})
+//Query Params - set them at the url
+app.get('/param', (req, res) =>{
+    res.send('This is a class ' + req.query.class + ' cohort ' + req.query.cohort)
+})
+
+
+//Error Page
+app.get('*', (req, res) => { res.send('Page not found');});
+
+
+
+
--- index.js
+++ index.js
@@ -1,31 +1,101 @@
-/*
-console.log("hello world")
-*/
-const express = require('express');
-const app  = express();
-app.listen(3000, function() {
-console.log('listening on 3000')
+//console.log('Hello world')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const path = require('path')
+app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(bodyParser.json());
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/node-demo");
+app.listen(3000, function () {
+    console.log('listening on 3000')
 })
-app.get('/', function (req, res){
-    res.send('Hello World')
+/* //app.get(path, callback)
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})/
+//app.get(path, callback)
+app.get('/about', (req, res) => {
+    res.send('about us page')
 })
-app.get('/about', function (req,res){
-    res.send('This page is About us')
+//post methods
+app.post('/', (req, res) => {
+    res.send('Got a post')
 })
-app.post ('/', function (req, res){
-    res.send('Got a POST request')
+// put method
+app.put('/', (req, res) => {
+    res.send('Got a put request at /user')
 })
-app.put ('/', function (req, res){
-    res.send('Got a PUT request')
+// delete method
+app.delete('/', (req, res) => {
+    res.send('Got a delete at /user')
 })
-app.delete ('/', function (req, res){
-    res.send('Got a DELETE request')
+// params
+app.get('/users/:name', (req, res) => {
+    res.send('hello  ' + req.params.name)
 })
-app.get('/users/:name',function (req, res){
-    res.send('hello' +  req.params.name)
+//error page
+//app.get('*', (req, res) => {
+// res.send('Dont worry about this we will sort it soon')
+//})
+app.get('/users', (req, res) => {
+    res.send('This is a class' + req.query.class + 'cohort' + req.query.cohort)
 })
-app.get('*',function(req,res){res.send('oops, The page doesn exist')}
-)
+
+ */
+
+app.get('/register_form', (req, res) => {
+    //    console.log('body',req.body)
+    //    console.log('query',req.query)
+    res.render('begin')
+})
+
+
+app.post('/newPath', (req, res) => {
+    //res.send("Hello " + req.body.firstname);
+    res.render('start', {
+        fname: req.body.firstname,
+        lname: req.body.lastname
+
+    });
+})
+// another method for post
+/* app.post('/register_form', (req, res) => {
+    console.log("form has been posted")
+    console.log('body', req.body)
+    console.log('query', req.query)
+    console.log('query params', req.query)
+    res.render('register_form')
+
+}) */
+var nameSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String
+});
+var User = mongoose.model("User", nameSchema);
 
 
 
+app.post('/newPath', (req, res) => {
+    // res.send('hello')
+    res.render("newForm", {
+        name: req.body.firstName,
+        gn: req.body.gender,
+    });
+})
+app.post("/register_form", (req, res) => {//express app
+    var myData = new User(req.body);//myData creates user(a module which )
+    myData.save()//module method
+        .then(item => {
+            res.send("item saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("unable to save to database");
+        });
+});
